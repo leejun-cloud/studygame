66adc22838a23a21e0b0945f873375f34ad2e2ee
[dyad] <duoenjia8@gmail.com>
2025-07-29 11:12:13 +0900

    [dyad] Fixing startup issue by simplifying Next.js config - wrote 1 file(s)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverComponentsExternalPackages: ["pdf-parse", "mammoth"],
-  webpack: (config) => {
-    if (process.env.NODE_ENV === "development") {
-      config.module.rules.push({
-        test: /\.(jsx|tsx)$/,
-        exclude: /node_modules/,
-        enforce: "pre",
-        use: "@dyad-sh/nextjs-webpack-component-tagger",
-      });
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
